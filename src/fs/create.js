63e08a6e91cd751db80202810a423aeebafbbdc3
Helen Kruminia <99581965+Lenoriya@users.noreply.github.com>
2023-06-16 15:22:34 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
 const create = async () => {
     // Write your code here 
+    
+    
+    var fs = require('fs');
+    var path = './fresh.txt';
+
+    fs.access(path, fs.F_OK, (err) => {
+  if (err) {
+    console.log('FS operation failed')
+    return
+  }
+  else {
+      fs.appendFile('fresh.txt', 'I am fresh and young', function (err) {
+  if (err) throw err;}
+  }
+
+  //file exists
+})
+
 };
 
 await create();
